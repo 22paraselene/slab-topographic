074e6fed56c1b9b7f460422ae899e1b32112de0f
22paraselene <354193874@qq.com>
2025-11-25 12:05:15 +0800

    update

--- src/main/java/slab_topographic/registry/ModBlocks.java
+++ src/main/java/slab_topographic/registry/ModBlocks.java
@@ -7,88 +7,112 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.SlabType;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraftforge.common.ToolAction;
+import net.minecraftforge.common.ToolActions;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import slab_topographic.slab_topographic;
 
 public class ModBlocks {
         public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
-                        slab_topographic.MODID);
+                slab_topographic.MODID);
 
         // 草方块台阶
         public static final RegistryObject<Block> GRASS_SLAB = BLOCKS.register("grass_slab",
-                        () -> new SlabBlock(BlockBehaviour.Properties.of()
-                                        .mapColor(MapColor.GRASS)
-                                        .strength(0.6F)
-                                        .sound(SoundType.GRASS)));
+                () -> new SlabBlock(BlockBehaviour.Properties.of()
+                        .mapColor(MapColor.GRASS)
+                        .strength(0.5F)
+                        .sound(SoundType.GRASS)
+                        .pushReaction(PushReaction.NORMAL)
+                        .isValidSpawn((state, getter, pos, type) -> true)));
 
         // 覆雪草方块台阶
         public static final RegistryObject<Block> SNOWY_GRASS_SLAB = BLOCKS.register("snowy_grass_slab",
-                        () -> new SlabBlock(BlockBehaviour.Properties.of()
-                                        .mapColor(MapColor.SNOW)
-                                        .strength(0.6F)
-                                        .sound(SoundType.SNOW)));
+                () -> new SlabBlock(BlockBehaviour.Properties.of()
+                        .mapColor(MapColor.SNOW)
+                        .strength(0.5F)
+                        .sound(SoundType.SNOW)
+                        .pushReaction(PushReaction.NORMAL)
+                        .isValidSpawn((state, getter, pos, type) -> true)) {
+                        @Override
+                        public @Nullable BlockState getToolModifiedState(BlockState state, net.minecraft.world.item.context.UseOnContext context, ToolAction toolAction, boolean simulate) {
+                                if (ToolActions.SHOVEL_FLATTEN.equals(toolAction)) {
+                                        return ModBlocks.GRASS_SLAB.get().defaultBlockState().setValue(TYPE, state.getValue(TYPE));
+                                }
+                                return super.getToolModifiedState(state, context, toolAction, simulate);
+                        }
+                });
 
         // 沙子台阶
         public static final RegistryObject<Block> SAND_SLAB = BLOCKS.register("sand_slab",
-                        () -> new SlabBlock(BlockBehaviour.Properties.of()
-                                        .mapColor(MapColor.SAND)
-                                        .strength(0.5F)
-                                        .sound(SoundType.SAND)));
+                () -> new SlabBlock(BlockBehaviour.Properties.of()
+                        .mapColor(MapColor.SAND)
+                        .strength(0.4F)
+                        .sound(SoundType.SAND)
+                        .pushReaction(PushReaction.NORMAL)));
 
         // 红沙台阶
         public static final RegistryObject<Block> RED_SAND_SLAB = BLOCKS.register("red_sand_slab",
-                        () -> new SlabBlock(BlockBehaviour.Properties.of()
-                                        .mapColor(MapColor.SAND)
-                                        .strength(0.5F)
-                                        .sound(SoundType.SAND)));
+                () -> new SlabBlock(BlockBehaviour.Properties.of()
+                        .mapColor(MapColor.COLOR_ORANGE)
+                        .strength(0.4F)
+                        .sound(SoundType.SAND)
+                        .pushReaction(PushReaction.NORMAL)));
 
         // 土径台阶
         public static final RegistryObject<Block> DIRT_PATH_SLAB = BLOCKS.register("dirt_path_slab",
-                        () -> new DirtPathSlabBlock(BlockBehaviour.Properties.of()
-                                        .mapColor(MapColor.DIRT)
-                                        .strength(0.6F)
-                                        .sound(SoundType.GRASS)));
+                () -> new DirtPathSlabBlock(BlockBehaviour.Properties.of()
+                        .mapColor(MapColor.DIRT)
+                        .strength(0.5F)
+                        .sound(SoundType.GRASS)
+                        .pushReaction(PushReaction.NORMAL)));
 
         // 砂砾台阶
         public static final RegistryObject<Block> GRAVEL_SLAB = BLOCKS.register("gravel_slab",
-                        () -> new SlabBlock(BlockBehaviour.Properties.of()
-                                        .mapColor(MapColor.DIRT)
-                                        .strength(0.6F)
-                                        .sound(SoundType.GRAVEL)));
+                () -> new SlabBlock(BlockBehaviour.Properties.of()
+                        .mapColor(MapColor.STONE)
+                        .strength(0.4F)
+                        .sound(SoundType.GRAVEL)
+                        .pushReaction(PushReaction.NORMAL)));
 
         // 灰化土台阶
         public static final RegistryObject<Block> PODZOL_SLAB = BLOCKS.register("podzol_slab",
-                        () -> new SlabBlock(BlockBehaviour.Properties.of()
-                                        .mapColor(MapColor.DIRT)
-                                        .strength(0.6F)
-                                        .sound(SoundType.GRASS)));
+                () -> new SlabBlock(BlockBehaviour.Properties.of()
+                        .mapColor(MapColor.COLOR_BROWN)
+                        .strength(0.5F)
+                        .sound(SoundType.GRASS)
+                        .pushReaction(PushReaction.NORMAL)));
 
         // 菌丝土台阶
         public static final RegistryObject<Block> MYCELIUM_SLAB = BLOCKS.register("mycelium_slab",
-                        () -> new SlabBlock(BlockBehaviour.Properties.of()
-                                        .mapColor(MapColor.DIRT)
-                                        .strength(0.6F)
-                                        .sound(SoundType.GRASS)));
+                () -> new SlabBlock(BlockBehaviour.Properties.of()
+                        .mapColor(MapColor.COLOR_PURPLE)
+                        .strength(0.5F)
+                        .sound(SoundType.GRASS)
+                        .pushReaction(PushReaction.NORMAL)));
 
         // 粘土台阶
         public static final RegistryObject<Block> CLAY_SLAB = BLOCKS.register("clay_slab",
-                        () -> new SlabBlock(BlockBehaviour.Properties.of()
-                                        .mapColor(MapColor.CLAY)
-                                        .strength(0.6F)
-                                        .sound(SoundType.SOUL_SAND)));
+                () -> new SlabBlock(BlockBehaviour.Properties.of()
+                        .mapColor(MapColor.CLAY)
+                        .strength(0.5F)
+                        .sound(SoundType.SOUL_SAND)
+                        .pushReaction(PushReaction.NORMAL)));
 
         // 泥土台阶
         public static final RegistryObject<Block> DIRT_SLAB = BLOCKS.register("dirt_slab",
-                        () -> new SlabBlock(BlockBehaviour.Properties.of()
-                                        .mapColor(MapColor.DIRT)
-                                        .strength(0.5F)
-                                        .sound(SoundType.GRAVEL)));
+                () -> new SlabBlock(BlockBehaviour.Properties.of()
+                        .mapColor(MapColor.DIRT)
+                        .strength(0.4F)
+                        .sound(SoundType.GRAVEL)
+                        .pushReaction(PushReaction.NORMAL)));
 
         // 自定义土径台阶类
         public static class DirtPathSlabBlock extends SlabBlock {
@@ -102,13 +126,22 @@ public class ModBlocks {
 
                 @Override
                 public @NotNull VoxelShape getShape(BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos,
-                                net.minecraft.world.phys.shapes.@NotNull CollisionContext context) {
+                                                    net.minecraft.world.phys.shapes.@NotNull CollisionContext context) {
                         SlabType slabtype = state.getValue(TYPE);
                         return switch (slabtype) {
                                 case DOUBLE -> DOUBLE_AABB;
                                 case TOP -> TOP_AABB;
                                 default -> BOTTOM_AABB;
                         };
+                }
+
+                @Override
+                public @Nullable BlockState getToolModifiedState(BlockState state, net.minecraft.world.item.context.UseOnContext context, ToolAction toolAction, boolean simulate) {
+                        if (ToolActions.SHOVEL_FLATTEN.equals(toolAction)) {
+                                // 当用锹右击时，可以转换为其他类型的台阶
+                                return ModBlocks.DIRT_SLAB.get().defaultBlockState().setValue(TYPE, state.getValue(TYPE));
+                        }
+                        return super.getToolModifiedState(state, context, toolAction, simulate);
                 }
         }
 }
--- src/main/java/slab_topographic/registry/ModItems.java
+++ src/main/java/slab_topographic/registry/ModItems.java
@@ -11,43 +11,46 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, slab_topographic.MODID);
 
+    // 所有台阶物品使用相同的属性
+    private static final Item.Properties SLAB_PROPERTIES = new Item.Properties();
+
     // 草方块台阶物品
     public static final RegistryObject<Item> GRASS_SLAB_ITEM = ITEMS.register("grass_slab",
-            () -> new BlockItem(ModBlocks.GRASS_SLAB.get(), new Item.Properties()));
+            () -> new BlockItem(ModBlocks.GRASS_SLAB.get(), SLAB_PROPERTIES));
 
     // 覆雪草方块台阶物品
     public static final RegistryObject<Item> SNOWY_GRASS_SLAB_ITEM = ITEMS.register("snowy_grass_slab",
-            () -> new BlockItem(ModBlocks.SNOWY_GRASS_SLAB.get(), new Item.Properties()));
+            () -> new BlockItem(ModBlocks.SNOWY_GRASS_SLAB.get(), SLAB_PROPERTIES));
 
     // 沙子台阶物品
     public static final RegistryObject<Item> SAND_SLAB_ITEM = ITEMS.register("sand_slab",
-            () -> new BlockItem(ModBlocks.SAND_SLAB.get(), new Item.Properties()));
+            () -> new BlockItem(ModBlocks.SAND_SLAB.get(), SLAB_PROPERTIES));
 
     // 红沙台阶物品
     public static final RegistryObject<Item> RED_SAND_SLAB_ITEM = ITEMS.register("red_sand_slab",
-            () -> new BlockItem(ModBlocks.RED_SAND_SLAB.get(), new Item.Properties()));
+            () -> new BlockItem(ModBlocks.RED_SAND_SLAB.get(), SLAB_PROPERTIES));
 
     // 土径台阶物品
     public static final RegistryObject<Item> DIRT_PATH_SLAB_ITEM = ITEMS.register("dirt_path_slab",
-            () -> new BlockItem(ModBlocks.DIRT_PATH_SLAB.get(), new Item.Properties()));
+            () -> new BlockItem(ModBlocks.DIRT_PATH_SLAB.get(), SLAB_PROPERTIES));
 
     // 砂砾台阶物品
     public static final RegistryObject<Item> GRAVEL_SLAB_ITEM = ITEMS.register("gravel_slab",
-            () -> new BlockItem(ModBlocks.GRAVEL_SLAB.get(), new Item.Properties()));
+            () -> new BlockItem(ModBlocks.GRAVEL_SLAB.get(), SLAB_PROPERTIES));
 
     // 灰化土台阶物品
     public static final RegistryObject<Item> PODZOL_SLAB_ITEM = ITEMS.register("podzol_slab",
-            () -> new BlockItem(ModBlocks.PODZOL_SLAB.get(), new Item.Properties()));
+            () -> new BlockItem(ModBlocks.PODZOL_SLAB.get(), SLAB_PROPERTIES));
 
     // 菌丝土台阶物品
     public static final RegistryObject<Item> MYCELIUM_SLAB_ITEM = ITEMS.register("mycelium_slab",
-            () -> new BlockItem(ModBlocks.MYCELIUM_SLAB.get(), new Item.Properties()));
+            () -> new BlockItem(ModBlocks.MYCELIUM_SLAB.get(), SLAB_PROPERTIES));
 
     // 粘土台阶物品
     public static final RegistryObject<Item> CLAY_SLAB_ITEM = ITEMS.register("clay_slab",
-            () -> new BlockItem(ModBlocks.CLAY_SLAB.get(), new Item.Properties()));
+            () -> new BlockItem(ModBlocks.CLAY_SLAB.get(), SLAB_PROPERTIES));
 
     // 泥土台阶物品
     public static final RegistryObject<Item> DIRT_SLAB_ITEM = ITEMS.register("dirt_slab",
-            () -> new BlockItem(ModBlocks.DIRT_SLAB.get(), new Item.Properties()));
+            () -> new BlockItem(ModBlocks.DIRT_SLAB.get(), SLAB_PROPERTIES));
 }
